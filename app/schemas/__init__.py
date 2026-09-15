dbd94fb48c0518b0ec42a0025a6f77a60a31821e
Basu19062001 <samantabasu.2001@gmail.com>
2026-09-16 01:57:31 +0530

    add document schemas to initialize module exports

---
+++ app/schemas/__init__.py
@@ -0,0 +1,13 @@
+from app.schemas.document import (
+    DocumentBase,
+    DocumentMetadata,
+    DocumentResponse,
+    DocumentStatus,
+)
+
+__all__ = [
+    "DocumentBase",
+    "DocumentMetadata",
+    "DocumentResponse",
+    "DocumentStatus",
+]
